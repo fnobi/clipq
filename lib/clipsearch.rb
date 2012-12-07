1babce96b3a26cf53035446864b35a4efc63f494
fnobi <shin@fnobi.com>
2012-12-07 15:05:54 +0900

    日本語も検索できるようにした。

--- lib/clipsearch.rb
+++ lib/clipsearch.rb
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+
+require 'uri'
 require 'parse_clipsearch'
 require 'format_search_result'
 
 def clipsearch (keyword)
+  keyword = URI.encode(keyword)
+
   url = "http://sfcclip.net/search?keyword=#{keyword}&titlesearch=on&leadsearch=on&bodysearch=on&date_st=&date_en=&category=&tag="
 
   html = open(url).read
